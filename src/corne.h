1f143a0a41e28eca9fcc9dca0d6748de315dc28c
Victor Voisin <victor.voisin@unicoda.fr>
2023-11-02 16:17:33 +0000

    chore: try to fix compilation error

--- src/corne.h
+++ src/corne.h
@@ -1,4 +1,19 @@
+// Based on https://github.com/manna-harbour/miryoku_zmk/blob/master/miryoku/miryoku.h
 
 #pragma once
+
+#define U_MACRO_VA_ARGS(macro, ...) macro(__VA_ARGS__)
+#define U_STRINGIFY(x) #x
+#define U_MACRO(name,...) \
+/ { \
+  macros { \
+    name: name { \
+      label = U_STRINGIFY(ZM_ ## name); \
+      compatible = "zmk,behavior-macro"; \
+      #binding-cells = <0>; \
+      __VA_ARGS__ \
+    }; \
+  }; \
+};
 
 #include "shift_functions.h"
