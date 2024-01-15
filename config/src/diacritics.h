2dadf251c57ea4c8170b896be83efa68165a4104
Victor Voisin <victor.voisin@unicoda.fr>
2024-01-16 00:06:17 +0100

    improve function and fix syntax

--- config/src/diacritics.h
+++ config/src/diacritics.h
@@ -5,18 +5,15 @@
 // Inspired by: https://github.com/urob/zmk-nodefree-config/blob/main/helper.h
 #define ZMK_HELPER_STRINGIFY(x) #x
 
-#define LETTER_ACUTE(name, letter_binding) \
+#define LETTER_ACUTE(name, diacritic_binding, letter_binding) \
 / { \
     macros { \
         name: name { \
-            compatible = "zmk,behavior-macro-one-param";
-            label = U_STRINGIFY(ZM_ ## name); \
-            wait-ms = <40>;
-            tap-ms = <40>;
-            #binding-cells = <1>;
-            bindings
-                = <&kp SINGLE_QUOTE>
-                , <letter_binding>;
+            compatible = "zmk,behavior-macro"; \
+            wait-ms = <40>; \
+            tap-ms = <40>; \
+            #binding-cells = <0>; \
+            bindings = <&kp diacritic_binding>, <&kp letter_binding>; \
         }; \
     }; \
 };
@@ -28,16 +25,16 @@
             compatible = "zmk,behavior-mod-morph"; \
             label = ZMK_HELPER_STRINGIFY(SHIFT_ ## name); \
             #binding-cells = <0>; \
-            bindings = <letter_binding>, <&kp LSHIFT letter_binding>; \
+            bindings = <&kp letter_binding>, <&kp LSHIFT &kp letter_binding>; \
             mods = <(MOD_LSFT|MOD_RSFT)>; \
         }; \
     }; \
 };
 
-#define GO_SHIFT_LETTER_ACUTE(name, binding) \
-SHIFT_LETTER_ACUTE(name ## _shifted, binding) \
-LETTER_ACUTE(name, &name ## _shifted)
+#define GO_SHIFT_LETTER_ACUTE(name, diacritic_binding, letter_binding) \
+SHIFT_LETTER_ACUTE(name ## _shifted, letter_binding) \
+LETTER_ACUTE(name, diacritic_binding, &name ## _shifted)
 
 
 
-GO_SHIFT_LETTER_ACUTE(fr_e_acute_test, &kp E)
+GO_SHIFT_LETTER_ACUTE(fr_e_acute_test, SINGLE_QUOTE, E)
