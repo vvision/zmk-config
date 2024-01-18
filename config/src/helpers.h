dfd2ea0010fe9cb0331f88f9d89e80d6b8facbf9
Victor Voisin <victor.voisin@unicoda.fr>
2024-01-18 19:13:14 +0100

    remove comment from function definition and add combo definition function

--- config/src/helpers.h
+++ config/src/helpers.h
@@ -3,17 +3,19 @@
 /**
  * Usage: HOLD_TAP(hm, 175, &kp, &kp)
  * &hm LSHIFT F
+ *
+ * tapping_term_ms: Sweet spot between 150 and 220
 */
 #define HOLD_TAP(name, tapping_term_ms, binding_function_1, binding_function_2) \
     \ { \
           compatible = "zmk,behavior-hold-tap"; \
           #binding-cells = <2>; \
-          // Sweet spot between 150 and 220
           quick_tap_ms = <0>; \
           tapping-term-ms = <tapping_term_ms>; \
           flavor = "tap-preferred"; \
           bindings = <binding_function_1>, <binding_function_2>; \
     };
+
 /**
  * Usage: HOLD_TAP_CUSTOM(hmc, 175, &kp, &custom_function)
  * &hmc RSHIFT
@@ -27,4 +29,16 @@
           tapping-term-ms = <tapping_term_ms>; \
           flavor = "tap-preferred"; \
           bindings = <binding_function>, <custom_function>; \
+    };
+
+/**
+ * Usage: COMBO(combo_ql_sqt, 20 21, &kp SQT, 0)
+*/
+#define COMBO(name, key_positions, bindings, layers) \
+    \ { \
+        timeout-ms = <50>;
+        // Hold K and L (in qwerty)
+        key-positions = <key_positions>;
+        bindings = <bindings>;
+        layers = <layers>;
     };
