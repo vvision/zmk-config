e6353727f3d61af11ef075f5ed1715c1d520c2ae
Victor Voisin <victor.voisin@unicoda.fr>
2024-02-28 13:48:34 +0000

    feat: configurable combos

--- config/src/mapping/34/ferris.h
+++ config/src/mapping/34/ferris.h
@@ -13,6 +13,8 @@ K10  K11  K12  K13  K14       K15  K16  K17  K18  K19 \
 K20  K21  K22  K23  K24       K25  K26  K27  K28  K29 \
                K33  K34       K35  K36
 
+// TODO Define combos
+
 #endif
 
 #define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_FERRIS
--- config/src/mapping/42/corne.h
+++ config/src/mapping/42/corne.h
@@ -15,6 +15,20 @@ XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
+/* Hold R and U for caps_word (in qwerty) */
+#define COMBOS_CAPS_WORDS 4 7
+
+/* Hold S and D for ` (in qwerty) */
+#define COMBOS_GRAVE 14 15
+/* Hold K and L for ' (in qwerty) */
+#define COMBOS_SINGLE_QUOTE 20 21
+
+/* Hold C and , for function layer (in qwerty) */
+#define COMBOS_FUNCTION_LAYER 27 32
+/* Hold V and M for ESCAPE (in qwerty) */
+#define COMBOS_ESCAPE_KEY 28 31
+
+
 #endif
 
 #define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_CORNE
