eacea4c0dd22fcfa28499b97dc12f5aa4ffe921e
Victor Voisin <victor.voisin@unicoda.fr>
2024-05-22 08:18:19 +0000

    feat: implement multi base layers available through combo

--- config/src/layers/layer_list.h
+++ config/src/layers/layer_list.h
@@ -3,25 +3,54 @@
 
 #if !defined (MIRYOKU_LAYER_LIST)
 
-#define MIRYOKU_LAYER_LIST \
-MIRYOKU_X(BASE, "Base") \
-MIRYOKU_X(SYM, "Symbol") \
-MIRYOKU_X(NAV, "Navigation") \
-MIRYOKU_X(NUM, "Numbers") \
-MIRYOKU_X(FUN, "Function") \
-MIRYOKU_X(ADJUST, "Adjust") \
-MIRYOKU_X(DIA, "Diacritics")
+  #if defined (MIRYOKU_MULTIPLE_ALPHAS)
 
-#define U_BASE   0
-#define U_SYM    1
-#define U_NAV    2
-#define U_NUM    3
-#define U_FUN    4
-#define U_ADJUST 5
-#define U_DIA    6
+    #define MIRYOKU_LAYER_LIST \
+    MIRYOKU_X(BASE, "Base") \
+    MIRYOKU_X(SYM, "Symbol") \
+    MIRYOKU_X(NAV, "Navigation") \
+    MIRYOKU_X(NUM, "Numbers") \
+    MIRYOKU_X(FUN, "Function") \
+    MIRYOKU_X(ADJUST, "Adjust") \
+    MIRYOKU_X(DIA, "Diacritics") \
+    MIRYOKU_X(BASE_ERGOL, "Ergol") \
+    MIRYOKU_X(BASE_COLEMAKDH, "ColemakDh") \
+    MIRYOKU_X(DIA_ERGOL, "DiaErgol")
 
-#if defined (MIRYOKU_ALPHAS_ERGOL)
-  #define U_DIA_ERGOL U_DIA
-#endif
+    #define U_BASE   0
+    #define U_SYM    1
+    #define U_NAV    2
+    #define U_NUM    3
+    #define U_FUN    4
+    #define U_ADJUST 5
+    #define U_DIA    6
+    #define U_BASE_ERGOL      7
+    #define U_BASE_COLEMAKDH  8
+    #define U_DIA_ERGOL       9
+
+  #else
+
+    #define MIRYOKU_LAYER_LIST \
+    MIRYOKU_X(BASE, "Base") \
+    MIRYOKU_X(SYM, "Symbol") \
+    MIRYOKU_X(NAV, "Navigation") \
+    MIRYOKU_X(NUM, "Numbers") \
+    MIRYOKU_X(FUN, "Function") \
+    MIRYOKU_X(ADJUST, "Adjust") \
+    MIRYOKU_X(DIA, "Diacritics")
+
+    #define U_BASE   0
+    #define U_SYM    1
+    #define U_NAV    2
+    #define U_NUM    3
+    #define U_FUN    4
+    #define U_ADJUST 5
+    #define U_DIA    6
+
+    #if defined (MIRYOKU_ALPHAS_ERGOL)
+      #define U_DIA_ERGOL U_DIA
+    #endif
+
+  #endif
 
 #endif
