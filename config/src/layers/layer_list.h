16a11a6292a13c569925218b7e2bcc081ec624dc
Victor Voisin <victor.voisin@unicoda.fr>
2024-05-24 13:18:06 +0000

    fix: reorder layers to have base layers first

--- config/src/layers/layer_list.h
+++ config/src/layers/layer_list.h
@@ -7,25 +7,25 @@
 
     #define MIRYOKU_LAYER_LIST \
     MIRYOKU_X(BASE, "Base") \
+    MIRYOKU_X(BASE_ERGOL, "Ergol") \
+    MIRYOKU_X(BASE_COLEMAKDH, "ColemakDh") \
     MIRYOKU_X(SYM, "Symbol") \
     MIRYOKU_X(NAV, "Navigation") \
     MIRYOKU_X(NUM, "Numbers") \
     MIRYOKU_X(FUN, "Function") \
     MIRYOKU_X(ADJUST, "Adjust") \
     MIRYOKU_X(DIA, "Diacritics") \
-    MIRYOKU_X(BASE_ERGOL, "Ergol") \
-    MIRYOKU_X(BASE_COLEMAKDH, "ColemakDh") \
     MIRYOKU_X(DIA_ERGOL, "DiaErgol")
 
     #define U_BASE   0
-    #define U_SYM    1
-    #define U_NAV    2
-    #define U_NUM    3
-    #define U_FUN    4
-    #define U_ADJUST 5
-    #define U_DIA    6
-    #define U_BASE_ERGOL      7
-    #define U_BASE_COLEMAKDH  8
+    #define U_BASE_ERGOL      1
+    #define U_BASE_COLEMAKDH  2
+    #define U_SYM    3
+    #define U_NAV    4
+    #define U_NUM    5
+    #define U_FUN    6
+    #define U_ADJUST 7
+    #define U_DIA    8
     #define U_DIA_ERGOL       9
 
   #else
