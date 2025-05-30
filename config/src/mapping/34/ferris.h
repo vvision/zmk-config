fc1325d586754058ab61272fc5ba2dcd44f00542
Victor Voisin <victor.voisin@unicoda.fr>
2025-05-30 19:07:09 +0200

    feat: aurora sweep build

--- config/src/mapping/34/ferris.h
+++ config/src/mapping/34/ferris.h
@@ -15,6 +15,37 @@ K20  K21  K22  K23  K24       K25  K26  K27  K28  K29 \
 
 // TODO Define combos
 
+/* LEFT part combo */
+
+/* Hold S and D for ` (in qwerty) */
+#define COMBOS_GRAVE 11 12
+/* Hold D and F for ESCAPE (in qwerty) */
+#define COMBOS_ESCAPE_KEY 12 13
+/* Hold S and F for TAB (in qwerty) */
+#define COMBOS_TAB_KEY 11 13
+
+
+/* RIGHT part combo */
+
+/* Hold K and L for ' (in qwerty) */
+#define COMBOS_SINGLE_QUOTE 17 18
+/* Hold J and K for BACKSPACE (in qwerty) */
+#define COMBOS_BACKSPACE 16 17
+
+
+/* BOTH part combo */
+
+/* Hold R and U for caps_word (in qwerty) */
+#define COMBOS_CAPS_WORDS 3 6
+/* Hold C and , for function layer (in qwerty) */
+#define COMBOS_FUNCTION_LAYER 22 27
+
+
+/* Combo for multi base layer */
+#define MIRYOKU_MULTIPLE_ALPHAS
+/* Hold Q and P to change base layer (in qwerty) */
+#define COMBOS_MULTI_LAYER 0 9
+
 #endif
 
 #define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_FERRIS
